6b6aafb2b83dfd1a7badc3ccd488bb2e02ee866f
PatrykRomanowski <71602577+PatrykRomanowski@users.noreply.github.com>
2022-08-21 23:01:37 +0200

    initial commit

---
+++ 01-starting-project/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderCardButton from "./HeaderCardButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="a table fullof delicious food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
